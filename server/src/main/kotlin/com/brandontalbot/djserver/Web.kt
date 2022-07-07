c45caad16e4cd1273380d806c7b2ca6be91a851a
Brandon Talbot <bjtal91@gmail.com>
2022-07-07 00:13:56 -0700

    Add a docker bundle

--- server/src/main/kotlin/com/brandontalbot/djserver/Web.kt
+++ server/src/main/kotlin/com/brandontalbot/djserver/Web.kt
@@ -1,26 +1,14 @@
 package com.brandontalbot.djserver
 
 import io.ktor.server.application.*
-import io.ktor.server.html.*
 import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
-import kotlinx.html.*
 
 fun Application.web() {
     routing {
-        get("/") {
-            call.respondHtml {
-                head {
-                    title("Brandon Talbot")
-                }
-                body {
-                    div { id = "root" }
-                    script(src = "/static/DJServer.js") {}
-                }
-            }
-        }
-        static("/static") {
+        static {
             resources()
+            defaultResource("index.html")
         }
     }
 }
